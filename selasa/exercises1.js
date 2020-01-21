22861cdeb6783df204182def2a89091c1ddc321b
ariiyantto168 <ariiyantto168@gmail.com>
2020-01-21 11:43:03 +0700

    week 4

---
+++ selasa/exercises1.js
@@ -0,0 +1,26 @@
+// Diberikan sebuah function angkaPrima(angka) yang menerima satu parameter berupa angka.
+//  Function akan me-return true jika angka tersebut adalah bilangan prima. Jika tidak, return false.
+
+// Angka Prima
+
+
+
+function angkaPrima(angka) {
+    // you can only write your code here!
+    var Hasil=true;  
+    for(i=2;i<angka;i++){
+    if(angka%i===0){
+      Hasil=false;
+      }
+  }
+  
+  return Hasil;
+
+  }
+  
+  // TEST CASES
+  console.log(angkaPrima(3)); // true
+  console.log(angkaPrima(7)); // true
+  console.log(angkaPrima(6)); // false
+  console.log(angkaPrima(23)); // true
+  console.log(angkaPrima(33)); // false
