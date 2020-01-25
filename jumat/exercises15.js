115e513abe34b6466200452f8953096e8479701f
ariiyantto168 <ariiyantto168@gmail.com>
2020-01-26 03:05:08 +0700

    updated exercises 15

--- jumat/exercises15.js
+++ jumat/exercises15.js
@@ -13,7 +13,42 @@ Output yang diharapkan berupa Object Literal dengan format sebagai berikut:
 */
 
 function highestScore (students) {
-    
+  var listClass = [];
+  var results = {};
+  
+  if (students.length === 0) {
+    console.log('');
+    return results;
+  }
+  
+  // Menentukan class
+  for (let i = 0; i < students.length; i++) {
+    var className = students[i].class;
+    if (listClass.includes(className)) {
+      continue;
+    } else {
+      listClass.push(className);
+    }
+  }
+  
+  
+  // Ranking nilai
+  for (let j = 0; j < listClass.length; j++) {
+    var objStudents = {};
+    var highest = 0;
+    for (let k = 0; k < students.length; k++) {
+      if (listClass[j] === students[k].class && students[k].score > highest) {
+        highest = students[k].score;
+        objStudents.name = students[k].name;
+        objStudents.score = students[k].score;
+      }
+      results[listClass[j]] = objStudents;
+    }
+  }
+  
+  console.log('');
+  return results;
+
 }
   
   // TEST CASE
