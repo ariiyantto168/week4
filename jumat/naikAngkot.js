66e97315ef4c944ddca0b17bfb4b0ddab08aca1d
ariiyantto168 <43541109+ariiyantto168@users.noreply.github.com>
2024-01-03 08:12:59 +0000

    Update naikAngkot.js

--- jumat/naikAngkot.js
+++ jumat/naikAngkot.js
@@ -12,44 +12,7 @@ Contoh: input: [['Dimitri', 'B', 'F']] output: [{ penumpang: 'Dimitri', naikDari
 
 function naikAngkot(arrPenumpang) {
     rute = ['A', 'B', 'C', 'D', 'E', 'F'];
-    let angkot = [{},{}];
-    let i=0;
-    let asal = '';
-    let tujuan = '';
-  
     
-    for (i; i<arrPenumpang.length; i++) {
-        let j = 0;
-        for (j; j<arrPenumpang[i].length; j++) {
-            switch (j) {
-                case 0: {
-                    angkot[i].penumpang = arrPenumpang[i][j];
-                    break;
-                } case 1: {
-                    angkot[i].naikDari = arrPenumpang[i][j];
-                    angkot[i].tujuan = arrPenumpang[i][j+1];
-                    break;
-                } case 2: {
-                    asal = arrPenumpang[i][j-1];
-                    tujuan = arrPenumpang[i][j];
-                    let jarak = 0;
-                    for (let k=0; k<rute.length; k++) {
-                        if (rute[k] === asal) {
-                            for (let l=k+1; l<rute.length; l++) {
-                                jarak += 1;
-                                if (rute[l] === tujuan) {
-                                    let bayar = jarak * 2000;
-                                    angkot[i].bayar = bayar;
-                                }
-                            }
-                        }
-                    }
-                    break;
-                }
-            }
-        }
-    }
-  return angkot;
 }
   
   //TEST CASE
